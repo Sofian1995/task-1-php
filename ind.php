8de376f9e32b447d1ce6faf0da9095c0dfe95413
Sofian1995 <sofia.almomani1995@gmail.com>
2022-02-05 11:21:45 +0200

    task1

---
+++ ind.php
@@ -0,0 +1,44 @@
+<?php
+echo"hello php";
+echo"<br>";
+?>
+
+<?php
+$name="ahmed";
+$age="12";
+echo $name;
+echo "<br>";
+echo $age;
+?>
+
+<!Doctype html>
+<html>
+ <h1>sofian</h1>   
+
+ ////task 2/////
+
+ <?php
+ echo"<br>";
+ $n = "do your task";
+ echo $n;
+ ?>
+<?php
+echo"<br>";
+echo strlen("what is you name?");?>
+<?php
+echo"<br>";
+echo str_word_count("my name is sofian");?>
+<?php
+echo"<br>";
+echo strrev("my name is sofian");?>
+<?php
+echo"<br>";
+echo strpo s("welcom to my task page","task");?>
+<?php
+echo"<br>";
+echo str_replace("farah","sara","this is sara page");?>
+<?php
+echo"<br>";
+define("name","farah");
+$name = "sara";
+echo name;?>
